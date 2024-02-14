dc6c8c46ea04b667e8e75e2f2b4c6ec50db560dd
AomSirawit <makdeesirawit@gmail.com>
2024-02-15 04:23:21 +0700

    last

--- src/app/Phetchabun/page.jsx
+++ src/app/Phetchabun/page.jsx
@@ -1,13 +1,86 @@
-import React from 'react'
+import React from "react";
 function Phetchabun() {
   return (
-    <div>
-          <h1 className="font-bold text-3xl text-center mt-5">อำเภอเมืองเพชรบูรณ์</h1>
-          <div className="h-screen">
-          <p className='text-center '>dddd</p>
-          </div>
+    <div
+      className="mx-auto text-center text-gray-700"
+      style={{ width: "700px" }}
+    >
+      <h1 className="font-bold text-3xl text-center mt-5">
+        อำเภอเมืองเพชรบูรณ์
+      </h1>
+      <div>
+        <h1 className="mx-5 mt-5 text-xl font-semibold">
+          1. พุทธอุทยานเพชบุระ
+        </h1>
+        <img
+          className="mt-5"
+          src="https://www.paiduaykan.com/travel/wp-content/uploads/2020/04/DSC_5775.jpg"
+        />
+        <p className=" text-gray-400">Cr. www.paiduaykan.com</p>
+        <p className="font-medium mt-5">
+          พุทธอุทยานเพชบุระ ตั้งอยู่ในอำเภอเมืองเพชรูบรณ์
+          มีมีพระพุทธมหาธรรมราชาเฉลิมพระเกียรติฯ
+          ซึ่งเป็นพระพุทธมหาธรรมราชาจำลององค์ใหญ่ที่สุดในโลก
+          พระพุทธรูปคู่บ้านคู่เมืองที่ชาวเพชรบูรณ์
+          ให้ความเคารพนับถือเป็นอย่างมาก ไม่มาไม่ได้แล้ว
+        </p>
+        <p className="font-semibold mt-5 text-lg">พิกัด</p>
+
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7653.107985458361!2d101.1502190884908!3d16.44746277978773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31203aa25e3edf43%3A0x8a84ec460acfe5ba!2z4Lie4Li44LiX4LiY4Lit4Li44LiX4Lii4Liy4LiZ4LmA4Lie4LiK4Lij4Lia4Li44Lij4Liw4LmA4Lie4LiK4Lij4Lia4Li54Lij4LiT4LmM!5e0!3m2!1sth!2sth!4v1707944085556!5m2!1sth!2sth"
+          style={{ width: "100%", height: "200px" }}
+          className="mt-5"
+        ></iframe>
+      </div>
+
+      <div>
+        <h1 className="mx-5 mt-5 text-xl font-semibold">
+          2. ศาลหลักเมืองเพชรบูรณ์
+        </h1>
+        <img
+          className="mt-5"
+          src="https://files.thailandtourismdirectory.go.th/assets/upload/info/attraction/5131/201710237cae30ff658eb4bd0dcafe0a6409cd64010830.jpg"
+        />
+        <p className=" text-gray-400">
+          Cr. https://thailandtourismdirectory.go.th/
+        </p>
+        <p className="font-medium mt-5">
+          ศาลหลักเมืองเพชรบูรณ์ ตั้งอยู่ใจกลางเมืองเพชรบูรณ์
+          เสาหลักเมืองเป็นเสาหิน เป็นศาลคู่บ้านคู่เมืองของเพชรบูรณ์
+          และเป็นที่ยึดเหนี่ยวจิดตใจของคนเพชรบูรณ์
+        </p>
+        <p className="font-semibold mt-5 text-lg">พิกัด</p>
+
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827.076135523176!2d101.15755667680817!3d16.420959229922662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31202555a4a7807d%3A0x68f92d50047a4c1a!2z4Lio4Liy4Lil4Lir4Lil4Lix4LiB4LmA4Lih4Li34Lit4LiH4LmA4Lie4LiK4Lij4Lia4Li54Lij4LiT4LmM!5e0!3m2!1sth!2sth!4v1707945062031!5m2!1sth!2sth"
+          style={{ width: "100%", height: "200px" }}
+          className="mt-5"
+        ></iframe>
+      </div>
+
+      <div>
+        <h1 className="mx-5 mt-5 text-xl font-semibold">3. ไร่กำนันจุล</h1>
+        <img
+          className="mt-5"
+          src="https://tatapi.tourismthailand.org/tatfs/Image/CustomPOI/Picture/P03025521_1.jpeg"
+        />
+        <p className=" text-gray-400">
+          Cr. https://thai.tourismthailand.org/
+        </p>
+        <p className="font-medium mt-5">
+        ก่อตั้งโดยกำนันจุล คุ้นวงศ์ ผู้บุกเบิกการทำไร่ส้มเขียวหวานส่งออกขายทั่วประเทศและประเทศเพื่อนบ้านรายแรก ๆ ของประเทศไทย ตั้งแต่ พ.ศ. 2479 ปัจจุบันเป็นสถานที่ท่องเที่ยวเชิงเกษตร บนเนื้อที่กว่า 10,000 ไร่ ลักษณะเป็นสวนเกษตรแบบผสมผสาน มีการปลูกต้นหม่อนเลี้ยงไหม ผลิตเส้นใยไหมสำหรับทอผ้า เริ่มจากการฟักไข่ไหมเป็นตัวหนอนและตัวดักแด้ จากนั้นจึงนำมาสาวใยไหมออกเป็นเส้น นอกจากนี้ยังมีการเลี้ยงปลาน้ำจืดหลายชนิด มีการจัดกิจกรรมท่องเที่ยว เช่น ชมการทำประมงน้ำจืด การแปรรูปปลา ชมสวนส้มโชกุน ส้มโอ ส้มเช้ง มัลเบอร์รี (หม่อน) การจัดการแหล่งน้ำ ชมการปลูกหม่อนเลี้ยงไหม การสาวไหมด้วยเทคโนโลยีที่ทันสมัย และจำหน่ายผลผลิตต่าง ๆ จากไร่บริเวณหน้าฟาร์ม เปิดทุกวัน เวลา 08.30-16.00 น.
+        </p>
+        <p className="font-semibold mt-5 text-lg">พิกัด</p>
+
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1915.0589916839588!2d101.0484706649123!3d16.26572254517206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31202095f94e86bb%3A0xe4b78acee65df708!2z4LmE4Lij4LmI4LiB4Liz4LiZ4Lix4LiZ4LiI4Li44LilIOC4quC4suC4guC4sjE!5e0!3m2!1sth!2sth!4v1707945334705!5m2!1sth!2sth"
+          style={{ width: "100%", height: "200px" }}
+          className="mt-5"
+        ></iframe>
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default Phetchabun
+export default Phetchabun;
